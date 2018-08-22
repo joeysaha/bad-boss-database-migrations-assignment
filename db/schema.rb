a979af2f173127df4c6519e0d81a8a8dc8da8ea3
Joey Saha <joeysaha@hotmail.com>
2018-08-22 00:07:57 -0400

    changed quantity column type from integer to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_033517) do
+ActiveRecord::Schema.define(version: 2018_08_22_035727) do
 
   create_table "locations", force: :cascade do |t|
     t.string "city"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_033517) do
     t.string "name"
     t.string "manufacturer"
     t.decimal "cost"
-    t.integer "quantity"
+    t.decimal "quantity"
   end
 
   create_table "widgets", force: :cascade do |t|
